d31cf320229bfae7eb937f7a562e7740381f2add
Johannes Günter Herforth <johannes.herforth@rwth-aachen.de>
2021-01-11 12:27:03 +0100

    Implemented all the functions required to pass all createCausalStructure tests - Ability to choose between alpha/heuristic causal output - Error handling when picking an invalid variant - Error handling when inputting an invalid EventLog - Returns causal structure in dictionary format

--- bin/pd/createCausalStructure.py
+++ bin/pd/createCausalStructure.py
@@ -1,5 +1,23 @@
 from pm4py.objects.log.log import EventLog
+from pm4py.algo.discovery.dfg import algorithm as dfg_discovery
+from pm4py.algo.discovery.causal import algorithm as causal_discovery
+from pm4py.algo.discovery.causal.variants import heuristic
+from pm4py.algo.discovery.footprints import algorithm as foot_discovery
 
 
 def generateCausalStructure(log: EventLog, variants: str = 'alpha') -> dict:
-    pass
+    if not isinstance(log, EventLog):
+        raise TypeError('Invalid Log Type')
+
+    causal = None
+
+    if variants == 'alpha':
+        causal = foot_discovery.apply(log, variant=foot_discovery.
+                                      Variants.ENTIRE_EVENT_LOG)
+    elif variants == 'heuristic':
+        dfg = dfg_discovery.apply(log)
+        causal = causal_discovery.apply(dfg, heuristic)
+    else:
+        raise TypeError("Variant is not supported.")
+
+    return causal

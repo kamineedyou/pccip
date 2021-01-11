8691bb8521596eb4ec714f984333800c6bda1ded
Johannes Günter Herforth <johannes.herforth@rwth-aachen.de>
2021-01-11 12:26:00 +0100

    Added all tests required for createCausalStructure to function as intended

---
+++ test/bin/pd-read-extend-causal/test_createCausalStructure.py
@@ -0,0 +1,30 @@
+import pytest
+from pccip.bin.pd.createCausalStructure import generateCausalStructure
+from pm4py.objects.log.importer.xes import importer as xes_importer
+
+
+class Test_CausalStructure:
+    @pytest.fixture
+    def validEventLog(self):
+        return xes_importer.apply('figure8.xes')
+
+    def test_ValidLogDefaults(self, validEventLog):
+        causal = generateCausalStructure(validEventLog)
+        assert isinstance(causal, dict)
+
+    def test_ValidLogVariantsParam(self, validEventLog):
+        causal = generateCausalStructure(validEventLog, variants='alpha')
+        causal2 = generateCausalStructure(validEventLog, variants='heuristic')
+        # Show that both variants provide dictionary outputs
+        assert isinstance(causal, dict)
+        assert isinstance(causal2, dict)
+        # Show that different variants give different outputs
+        assert causal != causal2
+        # See if the outputs are correct to previously known values
+        assert dict(causal['dfg'])[('a', 'b')] == 1
+        assert causal2[('a', 'b')] == 0.5
+
+    def test_InvalidLogDefaults(self):
+        # Use String as opposed to EventLog
+        with pytest.raises(TypeError):
+            generateCausalStructure('Process Model')

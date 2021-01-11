834fdf037271d4f2670a6a7c3dbd7000a19f86b2
Johannes Günter Herforth <johannes.herforth@rwth-aachen.de>
2021-01-11 12:35:05 +0100

    Added starting code for passageProcessDiscovery - Imports the event log - Extends the event log - Creates a causal net from the extended event log

--- bin/pd/PassageProcessDiscovery.py
+++ bin/pd/PassageProcessDiscovery.py
@@ -4,4 +4,13 @@ import createCausalStructure as ccs
 
 
 def passageProcessDiscovery(xes, cAlgo: str = 'alpha'):
-    pass
+    # import event log (through file path or already imported xes)
+    log = il.importEventLog(xes)
+
+    # extend the event log
+    ext_log = el.extendEventLog(log)
+
+    # generate causal structure of the event log
+    causal = ccs.generateCausalStructure(ext_log, variants=cAlgo)
+
+    return causal

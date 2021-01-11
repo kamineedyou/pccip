3742d7202e2efbbd1ac2e40bef0fde514038237a
Johannes Günter Herforth <jherforth1@gmail.com>
2021-01-11 12:15:00 +0100

    Completed importLog to pass all the tests - Function always returns a valid EventLog object - Ability to import from EventLog object or from xes file - many errors are accounted for (invalid input/file type)

--- bin/pd/importLog.py
+++ bin/pd/importLog.py
@@ -1,8 +1,19 @@
+from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py.objects.log.log import EventLog
+
 
 class WrongEventLogType(Exception):
     pass
 
 
 def importEventLog(xes) -> EventLog:
-    pass
+    if not isinstance(xes, str) and not isinstance(xes, EventLog):
+        raise TypeError("Invalid Event Log Type or Path")
+
+    if isinstance(xes, EventLog):
+        return xes
+
+    if ".xes" not in xes:
+        raise WrongEventLogType("Log type is not supported")
+
+    return xes_importer.apply(xes)

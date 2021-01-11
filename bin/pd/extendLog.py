52e407dbfd88bc385d5cc47916d65b98fa462bd0
Johannes Günter Herforth <johannes.herforth@rwth-aachen.de>
2021-01-11 12:22:51 +0100

    Implemented all the functions required to pass all extendLog tests - Generate Passage:Start Event - Generate Passage:END Event - Iterate through all traces and add both events to each trace - Handle invalid EventLog type logs

--- bin/pd/extendLog.py
+++ bin/pd/extendLog.py
@@ -1,13 +1,39 @@
+import datetime
 from pm4py.objects.log.log import Event, EventLog
 
 
 def generateStartTransition() -> Event:
-    pass
+    start = {'concept:name': 'Passage:START',
+             'lifecycle:transition': 'complete',
+             'time:timestamp':
+                 datetime.datetime(1, 1, 1, 0, 0,
+                                   tzinfo=datetime.timezone(
+                                       datetime.timedelta(seconds=3600)))
+             }
+
+    return Event(start)
 
 
 def generateEndTransition() -> Event:
-    pass
+    end = {'concept:name': 'Passage:END',
+           'lifecycle:transition': 'complete',
+           'time:timestamp':
+               datetime.datetime(9999, 12, 31, 0, 0,
+                                 tzinfo=datetime.timezone(
+                                     datetime.timedelta(seconds=3600)))
+           }
+
+    return Event(end)
 
 
 def extendEventLog(log: EventLog) -> EventLog:
-    pass
+    if not isinstance(log, EventLog):
+        raise TypeError("Invalid log type")
+
+    start = generateStartTransition()
+    end = generateEndTransition()
+    for trace in log:
+        trace.insert(0, start)
+        trace.append(end)
+
+    return log

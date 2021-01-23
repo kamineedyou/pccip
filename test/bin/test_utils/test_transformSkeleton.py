3a9a35e36251dd58bb6b9db420cb2902aa8ff8e5
unknown <barry.lehmann@rwth-aachen.de>
2021-01-23 18:15:16 +0100

    NetIntoSkeleton

---
+++ test/bin/test_utils/test_transformSkeleton.py
@@ -0,0 +1,69 @@
+from pccip.bin.utils.transformSkeleton import petriNetIntoSkeleton
+from pccip.bin.utils.transformSkeleton import UnlabeledTransitions
+from pm4py.objects.petri.petrinet import PetriNet, Marking
+from pm4py.objects.petri import utils
+from pytest import raises
+
+
+class TestSkeleton:
+
+    def test_PetriNetIntoSkeleton(self):
+        net = PetriNet("NotWorkflowNet")
+        source = PetriNet.Place("source")
+        source2 = PetriNet.Place("source2")
+        sink = PetriNet.Place("sink")
+        p_1 = PetriNet.Place("p_1")
+        # add them to the net
+        net.places.add(source)
+        net.places.add(sink)
+        net.places.add(p_1)
+        net.places.add(source2)
+        # create transitions
+        t_1 = PetriNet.Transition("name_1", "label_1")
+        t_2 = PetriNet.Transition("name_2", "label_2")
+        # Add the transitions to the Petri Net
+        net.transitions.add(t_1)
+        net.transitions.add(t_2)
+        # Add arcs
+        utils.add_arc_from_to(source, t_1, net)
+        utils.add_arc_from_to(t_1, p_1, net)
+        utils.add_arc_from_to(p_1, t_2, net)
+        utils.add_arc_from_to(t_2, sink, net)
+        utils.add_arc_from_to(source2, t_1, net)
+
+        # Adding tokens
+        initial_marking = Marking()
+        initial_marking[source] = 1
+        initial_marking[source2] = 1
+        final_marking = Marking()
+        final_marking[sink] = 1
+        skeleton = petriNetIntoSkeleton(net)
+        assert set(skeleton.edges) == set([(t_1, t_2)])
+
+    def test_NotLabeledException(self):
+        net = PetriNet("NotWorkflowNet")
+        source = PetriNet.Place("source")
+        source2 = PetriNet.Place("source2")
+        sink = PetriNet.Place("sink")
+        p_1 = PetriNet.Place("p_1")
+        # add them to the net
+        net.places.add(source)
+        net.places.add(sink)
+        net.places.add(p_1)
+        net.places.add(source2)
+        # create transitions
+        t_silent = PetriNet.Transition("silent", None)
+        t_1 = PetriNet.Transition("name_1", "label_1")
+        t_2 = PetriNet.Transition("name_2", "label_2")
+        # Add the transitions to the Petri Net
+        net.transitions.add(t_1)
+        net.transitions.add(t_2)
+        net.transitions.add(t_silent)
+        # Add arcs
+        utils.add_arc_from_to(source, t_1, net)
+        utils.add_arc_from_to(t_1, p_1, net)
+        utils.add_arc_from_to(p_1, t_2, net)
+        utils.add_arc_from_to(t_2, sink, net)
+        utils.add_arc_from_to(source2, t_1, net)
+        with raises(UnlabeledTransitions):
+            petriNetIntoSkeleton(net)

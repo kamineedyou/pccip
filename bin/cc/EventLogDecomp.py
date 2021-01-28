d78ed36551ba87aefb120d89554607a843a44064
yasmin_elsawy <yasmin_elsawy@hotmail.com>
2021-01-28 21:04:22 +0100

    Changes after review

--- bin/cc/EventLogDecomp.py
+++ bin/cc/EventLogDecomp.py
@@ -3,11 +3,24 @@ import os
 from pm4py.objects.log.util import basic_filter
 from pm4py.objects.log.log import  EventLog
 import pm4py
+from typing import List
+
+##
+from pm4py.objects.log.exporter.xes import exporter as xes_exporter
 
 
-def decompose_event_log(log , array_events) -> EventLog:
-    tracefilter_log_pos = basic_filter.filter_log_events_attr(log,array_events,
+
+def decompose_event_log(log:EventLog  , List_events:List[List]) -> EventLog:
+    filtered_log = basic_filter.filter_log_events_attr(log,List_events,
                                 parameters={basic_filter.Parameters.ATTRIBUTE_KEY: "concept:name",
                                 basic_filter.Parameters.POSITIVE: True})
 
-    return EventLog(tracefilter_log_pos)
+    return filtered_log
+
+
+log = xes_importer.apply('test_log.xes')
+List_events = ["a","c","b"]
+filtered_log = basic_filter.filter_log_events_attr(log,List_events,
+                        parameters={basic_filter.Parameters.ATTRIBUTE_KEY: "concept:name",
+                        basic_filter.Parameters.POSITIVE: True})
+xes_exporter.apply(filtered_log, 'filtered_log_test.xes')

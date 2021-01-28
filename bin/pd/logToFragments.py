1cbff9d06be5bf6e3cfc0600ad67b4ae97d2a9f5
Johannes Günter Herforth <johannes.herforth@rwth-aachen.de>
2021-01-28 12:01:36 +0100

    initial and final marking are now accounted for - initial marking of beginning fragment gets saved - final marking of final fragment gets saved - remove the artificial start and ending - add basic merge of fragments

--- bin/pd/logToFragments.py
+++ bin/pd/logToFragments.py
@@ -1,10 +1,10 @@
 from enum import Enum
 from pm4py.objects.log.log import EventLog, Trace
-from pm4py.objects.petri.petrinet import PetriNet
+from pm4py.objects.petri.petrinet import PetriNet, Marking
 from pm4py.algo.discovery.alpha.algorithm import apply as alpha_algo
 from pm4py.statistics.start_activities.log.get import get_start_activities
 from pm4py.statistics.end_activities.log.get import get_end_activities
-from pm4py.objects.petri.utils import remove_place
+from pm4py.objects.petri.utils import remove_place, merge, remove_transition
 
 
 def alpha_fragments(sublog: EventLog, parameters: dict = None) -> PetriNet:
@@ -41,7 +41,7 @@ class Variants(Enum):
     ALPHA = alpha_fragments
 
 
-def create_fragments(sublog: EventLog,
+def create_fragment(sublog: EventLog,
                      parameters: dict = None,
                      variant: Variants = Variants.DEFAULT_VARIANT) -> PetriNet:
     if parameters is None:
@@ -49,6 +49,44 @@ def create_fragments(sublog: EventLog,
 
     new_log = split_log(sublog)
     net, im, fm = variant(sublog=new_log, parameters=parameters)
-    remove_place(net, next(iter(im.keys())))
-    remove_place(net, next(iter(fm.keys())))
-    return net
+
+    initial_marking = Marking()
+    final_marking = Marking()
+    im_place = next(iter(im.keys()))
+    fm_place = next(iter(fm.keys()))
+
+    # Create new Marking if fragment is the start fragment
+    if len(im_place.out_arcs) == 1:
+        im_transition = next(iter(im_place.out_arcs)).target
+        if im_transition.label == 'Artificial:Start':
+            new_im_place = next(iter(im_transition.out_arcs)).target
+            remove_place(net, im_place)
+            remove_transition(net, im_transition)
+            initial_marking = Marking({new_im_place: 1})
+
+    # Create new Marking if fragment is the end fragment
+    if len(fm_place.in_arcs) == 1:
+        fm_transition = next(iter(fm_place.in_arcs)).source
+        if fm_transition.label == 'Artificial:End':
+            new_fm_place = next(iter(fm_transition.in_arcs)).source
+            remove_place(net, fm_place)
+            remove_transition(net, fm_transition)
+            final_marking = Marking({new_fm_place: 1})
+
+    if not len(initial_marking):
+        remove_place(net, im_place)
+    if not len(final_marking):
+        remove_place(net, fm_place)
+
+    return net, initial_marking, final_marking
+
+
+def merge_fragments(fragments: list) -> PetriNet:
+    final_net = merge(nets=fragments)
+    for net, im, fm in fragments:
+        if len(im):
+            initial_marking = im
+        if len(fm):
+            final_marking = fm
+
+    return final_net, initial_marking, final_marking

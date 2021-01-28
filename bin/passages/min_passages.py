52bffe745822f9b6cc57d79d54b165f39b65b17c
Johannes Günter Herforth <johannes.herforth@rwth-aachen.de>
2021-01-28 01:53:03 +0100

    revert min_passages change for other branch

--- bin/passages/min_passages.py
+++ bin/passages/min_passages.py
@@ -2,25 +2,22 @@ from pccip.bin.passages.passage import Passage
 from typing import Set, Tuple
 
 
-def algorithm(edges: Set[Tuple[str, str]], silents: Set[str]) -> Set[Passage]:
+def algorithm(edges: Set[Tuple[str, str]]) -> Set[Passage]:
     passage_set = set()
 
     while edges:
-        t_vis = set()
         p_prev = set()
         p_curr = set()
-        p_new = set()
         p_curr.add(edges.pop())
 
-        while len(p_curr) - len(p_prev):
+        while p_curr - p_prev:
             p_prev = p_curr
 
             for match in p_curr:
                 x = match[0]
                 y = match[1]
-                t_vis.update({x, y})
-                p_new = pi_both(x, y, edges, silents, t_vis)
-                p_curr = p_curr | p_new
+
+                p_curr = p_curr | pi_1(x, edges) | pi_2(y, edges)
             edges = edges - p_curr
 
         passage_set.add(Passage(p_curr))
@@ -28,10 +25,9 @@ def algorithm(edges: Set[Tuple[str, str]], silents: Set[str]) -> Set[Passage]:
     return passage_set
 
 
-def pi_both(x_activity: str, y_activity: str, edges: Set[Tuple[str, str]],
-            silents: Set[str], t_vis: Set[str]) -> Set[Tuple[str, str]]:
-    return {edge for edge in edges
-            if edge[0] == x_activity
-            or edge[1] == y_activity
-            or (edge[0] in silents and edge[0] in t_vis)
-            or (edge[1] in silents and edge[1] in t_vis)}
+def pi_1(x_activity: str, edges: Set[Tuple[str, str]]) -> Set[Tuple[str, str]]:
+    return {edge for edge in edges if edge[0] == x_activity}
+
+
+def pi_2(y_activity: str, edges: Set[Tuple[str, str]]) -> Set[Tuple[str, str]]:
+    return {edge for edge in edges if edge[1] == y_activity}

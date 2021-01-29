26981c719aba985676cf169cb879e19d0cfd7ce8
yasmin_elsawy <yasmin_elsawy@hotmail.com>
2021-01-29 10:59:15 +0100

    Changes to input data type

--- test/bin/EventLogDecomp/test_decompose_log.py
+++ test/bin/EventLogDecomp/test_decompose_log.py
@@ -6,8 +6,8 @@ from pm4py.objects.log.importer.xes import importer as xes_importer
 
 class Test_EventLogDecomp:
 
-    @pytest.mark.parametrize("List_events", [["a", "c", "b"]])
-    def test_validDecomp(self, List_events):
+    def test_validDecomp(self):
+        List_events = ["a", "c", "b"]
         currentDir = os.path.dirname(os.path.realpath(__file__))
         pathToFile = os.path.join(currentDir, 'test_log.xes')
         log = xes_importer.apply(pathToFile)
@@ -15,9 +15,6 @@ class Test_EventLogDecomp:
         pathToFileFilter = os.path.join(currentDir, 'filtered_log_test.xes')
         valid_log = xes_importer.apply(pathToFileFilter)
         filter_test = decompose_event_log(log, List_events)
-
-        assert len(filter_test) == len(valid_log)
-
         for case_index, case in enumerate(valid_log):
             for event_index, event in enumerate(case):
                 assert event == filter_test[case_index][event_index]

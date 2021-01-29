a5574f6c7b88e279358a38c6d84338c5673afeba
unknown <barry.lehmann@rwth-aachen.de>
2021-01-29 14:02:56 +0100

    now silent trans allowed

--- bin/utils/transformSkeleton.py
+++ bin/utils/transformSkeleton.py
@@ -2,15 +2,7 @@ from networkx import DiGraph
 from pm4py.objects.petri.petrinet import PetriNet
 
 
-class UnlabeledTransitions(Exception):
-    pass
-
-
 def petriNetIntoSkeleton(net: PetriNet) -> DiGraph:
-    for transation in net.transitions:
-        if transation.label is None:
-            raise UnlabeledTransitions(
-                "Petri is has silent transitions pls remove them before")
     skeleton = DiGraph()
     for place in net.places:
         for arcIn in place.in_arcs:

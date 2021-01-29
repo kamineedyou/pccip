7d287657331838f3937e39b551e7bc169f07931e
yasmin_elsawy <yasmin_elsawy@hotmail.com>
2021-01-29 10:58:08 +0100

    Changes to input data type

--- bin/cc/EventLogDecomp.py
+++ bin/cc/EventLogDecomp.py
@@ -3,7 +3,7 @@ from pm4py.objects.log.log import EventLog
 from typing import List
 
 
-def decompose_event_log(log: EventLog, List_events: List[List]) -> EventLog:
+def decompose_event_log(log: EventLog, List_events: List[str]) -> EventLog:
     c_name = "concept:name"
     par = {basic_filter.Parameters.ATTRIBUTE_KEY: c_name}
     par[basic_filter.Parameters.POSITIVE] = True

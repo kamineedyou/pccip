ef744658d1e4fe3e07279aeeb6f3355689c613a4
Johannes Günter Herforth <johannes.herforth@rwth-aachen.de>
2021-02-06 22:25:14 +0100

    added docstring to import log file

--- test/bin/importModel/test_ImportModel.py
+++ test/bin/importModel/test_ImportModel.py
@@ -21,5 +21,5 @@ class TestImportPetrinet:
     def test_WrongPath(self, filePath):
         currentDir = os.path.dirname(os.path.realpath(__file__))
         pathToFile = os.path.join(currentDir, filePath)
-        with pytest.raises(Exception):
+        with pytest.raises(TypeError):
             (net, initial_marking, final_marking) = importPetriNet(pathToFile)

d599c1587ed74dc718749bba1b8d6e14a462b887
yasmin <yasmin.elsawy@rwth-aachen.de>
2021-02-11 21:44:26 +0100

    Net fragments Test

--- test/bin/test_net_fragments/test_net_fragments.py
+++ test/bin/test_net_fragments/test_net_fragments.py
@@ -1,71 +1,81 @@
-# from pccip.bin.cc.net_fragments import net_fragments
-# import os
-# from pm4py.objects.log.importer.xes import importer as xes_importer
-# from pccip.bin.passages.passage import Passage
-# from pm4py.algo.discovery.alpha.algorithm import apply
-# from pm4py.objects.petri.importer import importer as pnml_importer
-# from pm4py.objects.petri.petrinet import PetriNet
+from pccip.bin.cc.net_fragments import create_net_fragments
+import os
+from pm4py.objects.log.importer.xes import importer as xes_importer
+from pm4py.algo.discovery.alpha.algorithm import apply
+from pm4py.objects.petri.importer import importer as pnml_importer
+from pm4py.objects.petri.petrinet import PetriNet
+from pccip.bin.cc.extendModel import extendModel
+from pccip.bin.utils.transformSkeleton import petriNetIntoSkeleton
+from pccip.bin.passages.min_passages import algorithm
 
 
-# class TestNnetFragmnets:
-#     pass
-# def test_validFragments(self):
+class TestNnetFragmnets:
 
-#     currentDir = os.path.dirname(os.path.realpath(__file__))
-#     pathToFile = os.path.join(currentDir, 'figure1.xes')
-#     log = xes_importer.apply(pathToFile)
-#     process_model, im, fm = apply(log)
-#     passage_list = [Passage({('h', 'sink'), ('g', 'sink')}),
-#                     Passage({('d', 'e'), ('c', 'e'), ('b', 'e')}),
-#                     Passage({('source', 'a')}),
-#                     Passage({('e', 'f'), ('e', 'h'), ('e', 'g')})]
+    def test_validFragments(self):
 
-#     net_fragments_test = net_fragments(passage_list, process_model, im, fm)
+        currentDir = os.path.dirname(os.path.realpath(__file__))
+        pathToFile = os.path.join(currentDir, 'figure1.xes')
+        log = xes_importer.apply(pathToFile)
+        process_model, im, fm = apply(log)
+        (extendedNet, extendedInitMarking, extendedFinalMarking) = extendModel(
+            process_model, im, fm)
+        skeletonGraph = petriNetIntoSkeleton(extendedNet)
+        transitionsOfExtendedNet = extendedNet.transitions
+        silentTransOfExtended = [
+            silentTrans for silentTrans
+            in transitionsOfExtendedNet if silentTrans.label is None]
+        passages = algorithm(skeletonGraph, silentTransOfExtended)
+        fragments_test = create_net_fragments(passages)
+        passages = list(passages)
+        true = True
+        for cnt, fragment_tuple_test in enumerate(fragments_test):
+            x, y = passages[cnt].getXY()
+            x = list(x)
+            if "Artificial:Start" in x:
+                idx = x.index("Artificial:Start")
+                print(idx)
+                x[idx] = "Start"
+            x = sorted(x)
 
-#     valid_fragments = []
-#     filenames = ['test_passage_g_h.pnml']
-#     filenames.append("test_passage_d_c_b.pnml")
-#     filenames.append("test_passage_source.pnml")
-#     filenames.append("test_passage_e.pnml")
+            y = list(y)
+            if "Artificial:End" in y:
+                idx = y.index("Artificial:End")
+                print(idx)
+                y[idx] = "End"
 
-#     for filename in filenames:
-#         pathToValid = os.path.join(currentDir, filename)
-#         fragment, im, fm = pnml_importer.apply(pathToValid)
-#         fragment = tuple([fragment, im, fm])
-#         valid_fragments.append(fragment)
+            y = sorted(y)
+            fragment_test, im_test, fm_test = fragment_tuple_test
+            filename = "passage_"+str(x)+str(y)+".pnml"
+            path = os.path.join(currentDir, filename)
+            fragment_valid, im_valid, fm_valid = pnml_importer.apply(path)
 
-#     for cnt, valid_fragment in enumerate(valid_fragments):
-#         true = True
-#         valid_net, valid_im, valid_fm = valid_fragment
-#         test_net, test_im, test_fm = net_fragments_test[cnt]
+            assert str(im_valid) == str(im_test)
+            assert str(fm_test) == str(fm_valid)
+            for arc in fragment_test.arcs:
+                found = False
+                if isinstance(arc.source, PetriNet.Place):
+                    arc_source_test = "place"
+                else:
+                    arc_source_test = arc.source.label
 
-#         assert str(valid_im) == str(test_im)
-#         assert str(valid_fm) == str(test_fm)
-#         for arc in test_net.arcs:
-#             found = False
-#             if isinstance(arc.source, PetriNet.Place):
-#                 arc_source_test = "place"
-#             else:
-#                 arc_source_test = arc.source.label
+                if isinstance(arc.target, PetriNet.Place):
+                    arc_sink_test = "place"
+                else:
+                    arc_sink_test = arc.target.label
 
-#             if isinstance(arc.target, PetriNet.Place):
-#                 arc_sink_test = "place"
-#             else:
-#                 arc_sink_test = arc.target.label
+                for arc_valid in fragment_valid.arcs:
+                    if isinstance(arc_valid.source, PetriNet.Place):
+                        arc_source_vaild = "place"
+                    else:
+                        arc_source_vaild = arc_valid.source.label
 
-#             for arc_valid in valid_net.arcs:
-#                 if isinstance(arc_valid.source, PetriNet.Place):
-#                     arc_source_vaild = "place"
-#                 else:
-#                     arc_source_vaild = arc_valid.source.label
+                    if isinstance(arc_valid.target, PetriNet.Place):
+                        arc_sink_vaild = "place"
+                    else:
+                        arc_sink_vaild = arc_valid.target.label
 
-#                 if isinstance(arc_valid.target, PetriNet.Place):
-#                     arc_sink_vaild = "place"
-#                 else:
-#                     arc_sink_vaild = arc_valid.target.label
-
-#                 if arc_source_test == arc_source_vaild:
-#                     if arc_sink_test == arc_sink_vaild:
-#                         found = True
-#                         break
-#             assert found == true
+                    if arc_source_test == arc_source_vaild:
+                        if arc_sink_test == arc_sink_vaild:
+                            found = True
+                            break
+                assert found == true

1c658dcb2d7cc53654961eddffa8555e33a880c5
Johannes Günter Herforth <johannes.herforth@rwth-aachen.de>
2021-02-12 21:57:11 +0100

    fixed test in test_passage_conformance - test did not check anything

--- test/passage_decomp/algorithm/test_passage_conformance.py
+++ test/passage_decomp/algorithm/test_passage_conformance.py
@@ -1,5 +1,6 @@
 from pccip.passage_decomp.cc.import_model import import_petri_net
 from pytest import fixture
+import pm4py
 from os import path
 from pm4py.objects.log.importer.xes import importer as log_importer
 from pccip.passage_decomp.algorithm.conformance_checking import passage_conformance_checking
@@ -8,14 +9,15 @@ from pccip.passage_decomp.algorithm.conformance_checking import passage_conforma
 class Test_conformance_passages:
     @fixture
     def alpha_model(self):
-        modelPath = "models/alphamodel.pnml"
+        # modelPath = "models/alphamodel.pnml"
         currentDir = path.dirname(path.realpath(__file__))
-        pathToModel = path.join(currentDir, modelPath)
+        # pathToModel = path.join(currentDir, modelPath)
         logPath = "logs/roadtraffic50traces.xes"
         pathToLog = path.join(currentDir, logPath)
 
-        net, init, final = import_petri_net(pathToModel)
+        # net, init, final = import_petri_net(pathToModel)
         log = log_importer.apply(pathToLog)
+        net, init, final = pm4py.discover_petri_net_inductive(log)
 
         return net, init, final, log
 
@@ -25,7 +27,7 @@ class Test_conformance_passages:
         init_marking = alpha_model[1]
         final_marking = alpha_model[2]
         log = alpha_model[3]
-        num = passage_conformance_checking(
+        _, global_fitness = passage_conformance_checking(
             net, init_marking, final_marking, log)
 
-        assert num  # == 1
+        assert global_fitness == 1
